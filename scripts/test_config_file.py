7b42ca0cedce72d1560135af737518f43ac0c1f6
Christophe GUYCHARD <cguychard@pennarbed.net>
2019-07-24 12:45:25 +0200

    [MIG] move to python3

--- scripts/test_config_file.py
+++ scripts/test_config_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 
@@ -8,12 +8,12 @@ import OdooScript
 class test_config_file(OdooScript.Script):
 
     def run(self):
-    
+
         odoo_host = self.getConfigValue('odoo_host')
         url = 'http://' + odoo_host + ':' + self.getConfigValue('odoo_port')
         db_name = self.getConfigValue('db_name')
         db_local = self.getConfigValue('db_local')
-    
+
         print "WILL USE ODOO SERVER : " + url + " \n \t AND DATABASE: " + db_name + "  (" + str(db_local) + ")\n"
 
 #*******************************************************
