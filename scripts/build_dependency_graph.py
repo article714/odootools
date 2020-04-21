b3f56e5a610642da39b4cb10ec73197a0903736c
xtof <christophe@certificare.fr>
2020-04-21 16:06:34 +0200

    [FIX] should be runnable

--- scripts/build_dependency_graph.py
+++ scripts/build_dependency_graph.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import odoo
-from odoo.tools import config
 from odootools import OdooScript
+
 try:
     import networkx
 except Exception:
@@ -15,21 +14,22 @@ class BuildDependencyGraph(OdooScript.Script):
     # ***********************************
     # Main
 
-    
     def run(self):
         if networkx is not None:
             # Build dependency graph
             graph = networkx.Graph()
-            mods = self.env["ir.module.module"].search([("state", "=", "installed")])
+            mods = self.env["ir.module.module"].search(
+                [("state", "=", "installed")]
+            )
             for mod in mods:
                 graph.add_node(mod.name)
                 for dep in mod.dependencies_id:
                     if dep.name not in graph.nodes():
                         graph.add_node(dep.name)
-                    graph.add_edge(mod.name,dep.name)
+                    graph.add_edge(mod.name, dep.name)
         else:
             self.logger.error("Cannot run without networkx")
-                
+
 
 # *******************************************************
 # Launch main function
